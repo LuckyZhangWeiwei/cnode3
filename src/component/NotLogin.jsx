021e51f5027fc4baff7934d0f30db1c8c7eeb660
LuckyZhangweiwei <zhangzhang_10@hotmail.com>
2018-12-26 11:55:49 +0800

    change style

--- src/component/NotLogin.jsx
+++ src/component/NotLogin.jsx
@@ -5,13 +5,13 @@ import PropTypes from 'prop-types';
 const NotLogin = (props) => {
   const link = `/user/login?search=${props.location.pathname}${props.location.search}`;
   return (
-    <div className="not-login-wrap tc bg-color">
-      <Link to={link} replace>
-        <p data-layout-align="center center" data-layout="layout" style={{ marginBottom: 0, color: '#fff' }}>
-          还未登录，现在就去登录
+    <Link to={link} replace>
+      <div className="not-login-wrap tc bg-color" style={{ height: '45px', lineHeight: '45px' }}>
+        <p data-layout-align="center center" data-layout="layout" style={{ color: '#fff' }}>
+            还未登录，现在就去登录
         </p>
-      </Link>
-    </div>
+      </div>
+    </Link>
   );
 };
 
